d9e272e52df946fe78c00402e6a62697ba19dc3a
Dave Barragan <itsdaveba@gmail.com>
2025-05-10 04:46:40 +0100

    Bump version: 1.0.0 → 1.0.1

--- src/cube_solver/__init__.py
+++ src/cube_solver/__init__.py
@@ -2,7 +2,7 @@
 
 __author__ = """Dave Barragan"""
 __email__ = 'itsdaveba@gmail.com'
-__version__ = '1.0.0'
+__version__ = '1.0.1'
 
 from cube_solver.cube import Cube
 from cube_solver.solver import Solver
